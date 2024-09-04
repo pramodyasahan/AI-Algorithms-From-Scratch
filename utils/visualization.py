bbb28a5d4e536e7298bdca17de161916afe07d07
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-09-05 04:37:48 +0530

    feat: implement regression plot

---
+++ utils/visualization.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+
+def plot_regression_line(X_test, y_test, w, b, feature_name='Feature', target_name='Target'):
+    plt.figure(figsize=(10, 6))
+
+    # Scatter plot of the feature vs target
+    plt.scatter(X_test, y_test, color='blue', label='Actual Data')
+
+    # Plot the regression line
+    plt.plot(X_test, X_test * w + b, color='red', label='Regression Line')
+
+    plt.xlabel(f'Normalized {feature_name}')
+    plt.ylabel(target_name)
+    plt.title(f'Regression Line for {feature_name} vs. {target_name}')
+    plt.legend()
+    plt.show()
