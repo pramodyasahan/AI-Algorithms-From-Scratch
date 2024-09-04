74cd9f45814c9a0fb2eb613c2114d1279a39f262
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-09-05 05:15:17 +0530

    Implement Linear Regression model training and evaluation with updated synthetic data

--- algorithms/linear_regression/train.py
+++ algorithms/linear_regression/train.py
@@ -1,10 +1,10 @@
 from sklearn import datasets
-from utils.validation_metrics import mean_squared_error, r_squared
+from sklearn.metrics import mean_squared_error, r2_score
 from utils.visualization import plot_regression_line
 from linear_regression import LinearRegression
 from sklearn.model_selection import train_test_split
 
-X, y = datasets.make_regression(n_samples=1000, n_features=1, noise=20, random_state=10)
+X, y = datasets.make_regression(n_samples=750, n_features=1, noise=10, random_state=42)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 regressor = LinearRegression(learning_rate=0.01)
@@ -13,6 +13,6 @@ y_pred = regressor.predict(X_test)
 
 plot_regression_line(X_test, y_test, y_pred)
 mse = mean_squared_error(y_test, y_pred)
-r2 = r_squared(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 print(f'MSE: {mse}')
 print(f'R2: {r2}')
