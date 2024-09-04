0e7a438460e8bc392f7b3c7790e4d14bf007e332
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-09-05 04:50:12 +0530

    fix some errors

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 
 
-def plot_regression_line(X_test, y_test, w, b, feature_name='Feature', target_name='Target'):
+def plot_regression_line(X_test, y_test, feature_name='Feature', target_name='Target'):
     plt.figure(figsize=(10, 6))
 
     # Scatter plot of the feature vs target
     plt.scatter(X_test, y_test, color='blue', label='Actual Data')
 
     # Plot the regression line
-    plt.plot(X_test, X_test * w + b, color='red', label='Regression Line')
+    plt.plot(X_test, X_test, color='red', label='Regression Line')
 
     plt.xlabel(f'Normalized {feature_name}')
     plt.ylabel(target_name)
