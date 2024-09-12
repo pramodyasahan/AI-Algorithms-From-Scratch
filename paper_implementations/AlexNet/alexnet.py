843543ae0f1277a53d5d6471cbf36e490eed7607
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-09-12 17:14:32 +0530

    Add Local Response Normalization and specify dropout rates in AlexNet implementation and implemented using PyTorch

--- paper_implementations/AlexNet/alexnet.py
+++ paper_implementations/AlexNet/alexnet.py
@@ -1,57 +1,67 @@
-from layers import *
-from utils import softmax
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
-class AlexNet:
-    """
-    AlexNet Model Class
-    """
+class LocalResponseNorm(nn.Module):
+    def __init__(self, size, alpha=1e-4, beta=0.75, k=2):
+        super(LocalResponseNorm, self).__init__()
+        self.size = size
+        self.alpha = alpha
+        self.beta = beta
+        self.k = k
 
+    def forward(self, x):
+        return F.local_response_norm(x, self.size, self.alpha, self.beta, self.k)
+
+
+class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
-        self.conv1 = ConvLayer(3, 96, kernel_size=11, stride=4, padding=2)
-        self.relu1 = ReLULayer()
-        self.pool1 = PoolingLayer(pool_size=3, stride=2)
+        super(AlexNet, self).__init__()
 
-        self.conv2 = ConvLayer(96, 256, kernel_size=5, stride=1, padding=2)
-        self.relu2 = ReLULayer()
-        self.pool2 = PoolingLayer(pool_size=3, stride=2)
+        self.features = nn.Sequential(
+            # Convolutional Layer 1
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
-        self.conv3 = ConvLayer(256, 384, kernel_size=3, stride=1, padding=1)
-        self.relu3 = ReLULayer()
+            # Local Response Normalization
+            LocalResponseNorm(5),
 
-        self.conv4 = ConvLayer(384, 384, kernel_size=3, stride=1, padding=1)
-        self.relu4 = ReLULayer()
+            # Convolutional Layer 2
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
-        self.conv5 = ConvLayer(384, 256, kernel_size=3, stride=1, padding=1)
-        self.relu5 = ReLULayer()
-        self.pool3 = PoolingLayer(pool_size=3, stride=2)
+            # Local Response Normalization
+            LocalResponseNorm(5),
 
-        self.fc1 = FullyConnectedLayer(256 * 6 * 6, 4096)
-        self.relu6 = ReLULayer()
-        self.dropout1 = DropoutLayer()
+            # Convolutional Layer 3
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
 
-        self.fc2 = FullyConnectedLayer(4096, 4096)
-        self.relu7 = ReLULayer()
-        self.dropout2 = DropoutLayer()
-        self.fc3 = FullyConnectedLayer(4096, num_classes)
+            # Convolutional Layer 4
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
 
-    def apply_layers(self, x, layers):
-        for layer in layers:
-            x = layer.forward(x)
+            # Convolutional Layer 5
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)  # Flatten
+        x = self.classifier(x)
         return x
-
-    def forward(self, input):
-        # Sequentially apply layers using the helper function
-        x = self.apply_layers(input, [self.conv1, self.relu1, self.pool1])
-        x = self.apply_layers(x, [self.conv2, self.relu2, self.pool2])
-        x = self.apply_layers(x, [self.conv3, self.relu3])
-        x = self.apply_layers(x, [self.conv4, self.relu4])
-        x = self.apply_layers(x, [self.conv5, self.relu5, self.pool3])
-
-        x = x.flatten()  # Flatten for fully connected layers
-
-        x = self.apply_layers(x, [self.fc1, self.relu6, self.dropout1])
-        x = self.apply_layers(x, [self.fc2, self.relu7, self.dropout2])
-        output = softmax(self.fc3.forward(x))
-
-        return output
