fix: error in gradient decent

algorithms/supervised_learning/linear_regression/linear_regression.py
@@ -44,8 +44,8 @@ class LinearRegression:
             y_pred = np.dot(X, self.weights) + self.biases
 
             # Compute gradients
-            dw = (2 / n_samples) * np.dot(X.T, (y_pred - y))
-            db = (2 / n_samples) * np.sum(y_pred - y)
+            dw = (-2 / n_samples) * np.dot(X.T, (y - y_pred))
+            db = (-2 / n_samples) * np.sum(y - y_pred)
 
             # Update parameters
             self.weights -= self.learning_rate * dw